31d5feb706a3614ccb3815ad96de5cbe14e723d6
bhattigurjot <bhattigurjot@gmail.com>
2022-03-25 00:04:08 -0600

    Added check for invalid command call

--- cli.py
+++ cli.py
@@ -6,10 +6,14 @@ import subprocess
 TOOL_DIR = "tools"
 
 def run_command(cmd):
-    subprocess.call(["python", "{}/{}/{}.py".format(os.getcwd(), TOOL_DIR, cmd)])
+    script = "{}/{}/{}.py".format(os.getcwd(), TOOL_DIR, cmd)
+    if (os.path.exists(script)):
+        print("Executing: ", cmd)
+        subprocess.call(["python", script])
+    else:
+        print("Invalid command: ", cmd)
 
 for i in range(1, len(sys.argv)):
     cmd = sys.argv[i]
     print("\n-------------------")
-    print("Executing: ", cmd)
     run_command(cmd)
