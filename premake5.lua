fce769b836a4f0e66bf09572eb117d2122f0819f
bhattigurjot <bhattigurjot@gmail.com>
2022-03-26 21:33:59 -0600

    Updated premake to include engine project and links to editor

--- premake5.lua
+++ premake5.lua
@@ -7,13 +7,38 @@ workspace "infinite"
 tdir = "bin/%{cfg.buildcfg}/%{prj.name}"
 odir = "bin_obj/%{cfg.buildcfg}/%{prj.name}"
 
+project "infinite"
+    location "infinite"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+    targetdir(tdir)
+    objdir(odir)
+    files 
+    {
+        "%{prj.name}/include/**.h",
+        "%{prj.name}/src/**.h", 
+        "%{prj.name}/src/**.cpp"
+    }
+    sysincludedirs { "%{prj.name}/include/%{prj.name}" }
+    flags { "FatalWarnings" }
+    
 project "infinite-editor"  
     location "infinite-editor"   
     kind "ConsoleApp"   
     language "C++"   
     cppdialect "C++17"
+    staticruntime "on"
+    links { "infinite" }
     targetdir(tdir)
     objdir(odir)
-    files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp" }
+    files 
+    {
+        "%{prj.name}/include/**.h",
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp" 
+    }
+    sysincludedirs { "infinite/include", "infinite/src" }
     flags { "FatalWarnings" }
     
