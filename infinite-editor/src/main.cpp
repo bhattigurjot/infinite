c6848e01fd2fd10862f935133f15e9c44996575d
bhattigurjot <bhattigurjot@gmail.com>
2022-03-28 21:22:14 -0600

    Added cross platform cin.ignore instead of system("pause")

--- infinite-editor/src/main.cpp
+++ infinite-editor/src/main.cpp
@@ -6,8 +6,11 @@ int main()
     infinite::GetInfo();
     infinite::Initialize();
     infinite::Shutdown();
-#ifdef INFINITE_PLATFORM_WINDOWS
-    system("pause");
-#endif
+
+// #ifdef INFINITE_PLATFORM_WINDOWS
+//     system("pause");
+// #endif
+    std::cout << "Press ENTER to continue.." << std::endl;
+    std::cin.ignore();
     return 0;
 }
